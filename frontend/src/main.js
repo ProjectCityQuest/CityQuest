1a66fdfe0e588ae8c947f1ff3378598d831e85d4
Quantompixel <39237785+Quantompixel@users.noreply.github.com>
2023-03-10 15:53:34 +0100

    register page with vuelidate

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,8 @@ import router from './router'
 
 const app = createApp(App)
 
+import './assets/base.css'
+
 app.use(router)
 
 app.mount('#app')
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LoginView from "@/components/views/LoginView.vue";
-import LandingView from "@/components/views/LandingView.vue";
-import RegisterView from "@/components/views/RegisterView.vue";
+import LoginView from "@/views/LoginView.vue";
+import LandingView from "@/views/LandingView.vue";
+import RegisterView from "@/views/RegisterView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
