650a72a9038392ab2712a35c0eb750953edb2fcd
Quantompixel <39237785+Quantompixel@users.noreply.github.com>
2023-03-10 13:36:07 +0100

    deleted all unnecessary files in frontend

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,18 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import LoginView from "@/components/views/LoginView.vue";
+import LandingView from "@/components/views/LandingView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home'
+      name: 'landing',
+      component: LandingView
     },
     {
-      path: '/about',
-      name: 'about'
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
+      path: '/login',
+      name: 'login',
+      component: LoginView
     }
   ]
 })
