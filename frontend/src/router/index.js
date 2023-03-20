1d622e77e0553ee02874c93afd636780b1637018
Daniel Pillwein <9132@htl.rennweg.at>
2023-03-20 14:22:01 +0100

    added checkSessionKey() function

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import LoginView from "@/views/LoginView.vue";
 import LandingView from "@/views/LandingView.vue";
 import RegisterView from "@/views/RegisterView.vue";
@@ -6,73 +6,87 @@ import EmailView from "@/views/EmailView.vue";
 import MapView from "@/views/MapView.vue";
 
 function getCookie(cname) {
-  let name = cname + "=";
-  let decodedCookie = decodeURIComponent(document.cookie);
-  let ca = decodedCookie.split(';');
-  for(let i = 0; i <ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
     }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
+    return "";
+}
+
+async function checkSessionKey() {
+    const response = await fetch('http://127.0.0.1:8080/api/users', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'sessionKey': getCookie('sessionKey')
+        },
+        withCredentials: true,
+        credentials: 'same-origin'
+    })
+    return response.status === 200;
 }
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'landing',
-      component: LandingView
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: LoginView
-    },
-    {
-      path: '/registrierung',
-      name: 'registrierung',
-      component: RegisterView
-    },
-    {
-      path: '/email-verifizierung/:email/:verificationKey',
-      name: 'email-verifizierung-checking',
-      component: EmailView,
-      props: true
-    },
-    {
-      path: '/email-verifizierung/:email',
-      name: 'email-verifizierung-pending',
-      component: EmailView,
-      props: true
-    },
-    {
-      path: '/map',
-      name: 'map',
-      component: MapView,
-      meta: {
-        requiresAuth: true
-      }
-    }
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'landing',
+            component: LandingView
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: LoginView
+        },
+        {
+            path: '/registrierung',
+            name: 'registrierung',
+            component: RegisterView
+        },
+        {
+            path: '/email-verifizierung/:email/:verificationKey',
+            name: 'email-verifizierung-checking',
+            component: EmailView,
+            props: true
+        },
+        {
+            path: '/email-verifizierung/:email',
+            name: 'email-verifizierung-pending',
+            component: EmailView,
+            props: true
+        },
+        {
+            path: '/map',
+            name: 'map',
+            component: MapView,
+            meta: {
+                requiresAuth: true
+            }
+        }
+    ]
 })
 
-router.beforeEach((to, from, next) => {
-  if (to.matched.some(record => record.meta.requiresAuth)) {
-    console.log(getCookie('sessionKey'))
-    if (false){
-      next()
+router.beforeEach(async (to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        if (await checkSessionKey()) {
+            next()
+        } else {
+            next({
+                path: '/login'
+            })
+        }
     } else {
-      next({
-        path: '/login'
-      })
+        next()
     }
-  } else { next() }
 })
 
 export default router
