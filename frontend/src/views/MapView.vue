668599a78ca191a242178904e0ac5c562c2097c6
Quantompixel <39237785+Quantompixel@users.noreply.github.com>
2023-04-06 11:12:04 +0200

    structured code

--- frontend/src/views/MapView.vue
+++ frontend/src/views/MapView.vue
@@ -25,129 +25,134 @@ export default {
   name: 'MapView',
   data() {
     return {
-      positionFound: false,
-      userPositionFound: false
+      userPositionFound: false,
+      map: undefined,
+      vectorSource: undefined,
+      vectorLayer: undefined,
+      locateButton: undefined
+    }
+  },
+  methods: {
+    initiateMap(enablePreloading) {
+      this.map = new Map({
+        target: 'map', layers: [new TileLayer({
+          // enables preloading (blurry low res tiles)
+          preload: enablePreloading ? Infinity : 0,
+          source: new OSM()
+        })], view: new View({
+          center: [0, 0], zoom: 2
+        })
+      });
+    },
+    trackUserPosition() {
+      navigator.geolocation.watchPosition(
+          (pos) => {
+            // centers map on user position
+            if (!this.userPositionFound) {
+              try {
+                this.map.getView().fit(this.vectorSource.getExtent(), {
+                  maxZoom: 18,
+                  duration: 500,
+                });
+
+                this.userPositionFound = true;
+              } catch (err) {
+                console.log("User not located yet")
+              }
+            }
+
+            const coords = [pos.coords.longitude, pos.coords.latitude];
+            const accuracy = circular(coords, pos.coords.accuracy);
+            this.vectorSource.clear(true);
+            this.vectorSource.addFeatures([
+              new Feature(
+                  accuracy.transform('EPSG:4326', this.map.getView().getProjection())
+              ),
+              new Feature(new Point(fromLonLat(coords))),
+            ]);
+          },
+          function (error) {
+            alert(`ERROR: ${error.message}`);
+          },
+          {
+            enableHighAccuracy: true,
+          }
+      );
+    },
+    createLocateButton() {
+      this.locate = document.createElement('div');
+      this.locate.className = 'ol-control ol-unselectable locate';
+      this.locate.innerHTML = '<button title="Locate me">◎</button>';
+      this.locate.addEventListener('click', () => {
+        if (!this.vectorSource.isEmpty()) {
+          this.map.getView().fit(this.vectorSource.getExtent(), {
+            maxZoom: 18,
+            duration: 500,
+          });
+        }
+      });
+      this.map.addControl(
+          new Control({
+            element: this.locate,
+          })
+      );
+    },
+    trackUserHeading() {
+      const style = new Style({
+        stroke: new Stroke({
+          color: 'rgba(237, 54, 36, 0.6)',
+          lineDash: [4, 8]
+        }),
+        image: new Icon({
+          src: '/src/assets/location-heading.svg',
+          imgSize: [24, 24],
+          rotateWithView: true,
+        }),
+      });
+
+      this.vectorLayer.setStyle(style)
+
+      function startCompass() {
+        kompas()
+            .watch()
+            .on('heading', function (heading) {
+              style.getImage().setRotation((Math.PI / 180) * heading);
+            });
+      }
+
+      if (
+          window.DeviceOrientationEvent &&
+          typeof DeviceOrientationEvent.requestPermission === 'function'
+      ) {
+        this.locate.addEventListener('click', function () {
+          DeviceOrientationEvent.requestPermission()
+              .then(startCompass)
+              .catch(function (error) {
+                alert(`ERROR: ${error.message}`);
+              });
+        });
+      } else if ('ondeviceorientationabsolute' in window) {
+        startCompass();
+      } else {
+        alert('No device orientation provided by device');
+      }
     }
   },
   mounted() {
-    const map = new Map({
-      target: 'map', layers: [new TileLayer({
-        // enables preloading (blurry low res tiles)
-        preload: Infinity,
-        source: new OSM()
-      })], view: new View({
-        center: [0, 0], zoom: 2
-      })
+    this.initiateMap(true)
+
+    this.vectorSource = new VectorSource();
+    this.vectorLayer= new VectorLayer({
+      source: this.vectorSource,
     });
 
-    const source = new VectorSource();
-    const layer = new VectorLayer({
-      source: source,
-    });
+    this.map.addLayer(this.vectorLayer)
 
-    map.addLayer(layer)
+    this.trackUserPosition()
 
-    /*
-     Draws GPS position on the map
-     */
-    navigator.geolocation.watchPosition(
-        (pos) => {
-          // centers map on user position
-          if (!this.userPositionFound) {
-            try {
-              map.getView().fit(source.getExtent(), {
-                maxZoom: 18,
-                duration: 500,
-              });
+    this.createLocateButton()
 
-              this.userPositionFound = true;
-            } catch (err) {
-              console.log("User not located yet")
-            }
-          }
-
-          const coords = [pos.coords.longitude, pos.coords.latitude];
-          const accuracy = circular(coords, pos.coords.accuracy);
-          source.clear(true);
-          source.addFeatures([
-            new Feature(
-                accuracy.transform('EPSG:4326', map.getView().getProjection())
-            ),
-            new Feature(new Point(fromLonLat(coords))),
-          ]);
-        },
-        function (error) {
-          alert(`ERROR: ${error.message}`);
-        },
-        {
-          enableHighAccuracy: true,
-        }
-    );
-
-    /*
-     Locate button to instantly move to GPS position
-     */
-    const locate = document.createElement('div');
-    locate.className = 'ol-control ol-unselectable locate';
-    locate.innerHTML = '<button title="Locate me">◎</button>';
-    locate.addEventListener('click', function () {
-      if (!source.isEmpty()) {
-        map.getView().fit(source.getExtent(), {
-          maxZoom: 18,
-          duration: 500,
-        });
-      }
-    });
-    map.addControl(
-        new Control({
-          element: locate,
-        })
-    );
-
-    /*
-     Displays direction of device
-     */
-
-    const style = new Style({
-      stroke: new Stroke({
-        color: 'rgba(237, 54, 36, 0.6)',
-        lineDash: [4, 8]
-      }),
-      image: new Icon({
-        src: '/src/assets/location-heading.svg',
-        imgSize: [24, 24],
-        rotateWithView: true,
-      }),
-    });
-
-    layer.setStyle(style)
-
-    map.addLayer(layer)
-
-    function startCompass() {
-      kompas()
-          .watch()
-          .on('heading', function (heading) {
-            style.getImage().setRotation((Math.PI / 180) * heading);
-          });
-    }
-
-    if (
-        window.DeviceOrientationEvent &&
-        typeof DeviceOrientationEvent.requestPermission === 'function'
-    ) {
-      locate.addEventListener('click', function () {
-        DeviceOrientationEvent.requestPermission()
-            .then(startCompass)
-            .catch(function (error) {
-              alert(`ERROR: ${error.message}`);
-            });
-      });
-    } else if ('ondeviceorientationabsolute' in window) {
-      startCompass();
-    } else {
-      alert('No device orientation provided by device');
-    }
+    this.trackUserHeading()
   },
 }
 </script>
