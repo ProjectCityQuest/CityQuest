405b6bdc93faf79f896462dca6795caceec82432
Daniel Pillwein <9132@htl.rennweg.at>
2023-04-17 16:37:48 +0200

    improved setup for delete account view

--- frontend/src/views/AccountView.vue
+++ frontend/src/views/AccountView.vue
@@ -50,19 +50,6 @@
       </div>
       <p class="overlay-error">{{ getLogoutError }}</p>
     </Overlay>
-    <Overlay :is-visible="deleteAccountOverlayVisible" @close-overlay="deleteAccountOverlayVisible=false">
-      <h1 class="overlay-header">Account löschen</h1>
-      <p class="overlay-info">
-        Bist du dir sicher, dass du deinen Account löschen möchtest?
-      </p>
-      <p class="overlay-info">
-        Dein Puzzlefortschritt und deine Sammelbuch-Einträge gehen verloren.
-      </p>
-      <div class="action-wrapper">
-        <button class="overlay-action" @click="requestAccountDelete">Account löschen</button>
-      </div>
-      <p class="overlay-error">{{ getDeleteAccountError }}</p>
-    </Overlay>
     <NavBar :active-icon="1"></NavBar>
   </div>
 </template>
--- frontend/src/views/deleteAccountView.vue
+++ frontend/src/views/deleteAccountView.vue
@@ -2,9 +2,65 @@
 
 </template>
 
+
 <script>
+import NavBar from "@/components/NavBar.vue";
+import Overlay from "@/components/Overlay.vue";
+
 export default {
-  name: "deleteAccountView.vue"
+  name: "deleteAccountView",
+  components: {},
+  data() {
+    return {
+      logOutOverlayVisible: false,
+      deleteAccountOverlayVisible: false,
+      userData: {},
+      errors: {}
+    }
+  },
+  methods: {
+    async requestAccountDelete() {
+      const response = await fetch(`http://${window.location.hostname}:8080/api/deleteusers`, {
+        method: 'DELETE',
+        headers: {
+          sessionKey: this.getCookie('sessionKey')
+        },
+        withCredentials: true,
+        credentials: 'same-origin'
+      });
+
+      if (response.ok) {
+        this.$router.push("/")
+        // deletes cookie
+        document.cookie = "sessionKey= ; expires = Thu, 01 Jan 1970 00:00:00 GMT"
+      } else {
+        this.deleteAccountError(response)
+      }
+    },
+    deleteAccountError(response) {
+      if (response.ok) return
+      if (response.status === 400) {
+        response.json().then(data => this.errors.deleteAccount = data.error)
+      } else {
+        this.errors.deleteAccount = 'Das hat nicht geklappt ):'
+      }
+    },
+    getCookie(cname) {
+      let name = cname + "=";
+      let decodedCookie = decodeURIComponent(document.cookie);
+      let ca = decodedCookie.split(';');
+      for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
+          c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+          return c.substring(name.length, c.length);
+        }
+      }
+      return false;
+    }
+  }
 }
 </script>
 
