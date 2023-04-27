b66f5707d3c4d24108a048071c6235934d66be5c
Markus Wizany <9143@htl.rennweg.at>
2023-04-27 22:05:23 +0200

    init + updated create script

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -1,18 +1,21 @@
 DROP TABLE IF EXISTS Users;
 DROP TABLE IF EXISTS Bewertung;
 
-create table Users (
-                      pk_id INTEGER primary key auto_increment,
-                      username varchar(50),
-                      password varchar(50),
-                      email varchar(50),
-                      email_is_verified boolean
+create table Users
+(
+    pk_id             INTEGER primary key auto_increment,
+    username          varchar(50),
+    password          varchar(50),
+    email             varchar(50),
+    email_is_verified boolean,
+    profile_picture   varchar(50000)
 );
 
-create table Bewertung (
-                           pk_id INTEGER primary key auto_increment,
-                           design INTEGER,
-                           navigation INTEGER,
-                           puzzle INTEGER,
-                           sammelbuch INTEGER
+create table Bewertung
+(
+    pk_id      INTEGER primary key auto_increment,
+    design     INTEGER,
+    navigation INTEGER,
+    puzzle     INTEGER,
+    sammelbuch INTEGER
 );
