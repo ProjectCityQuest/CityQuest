970e7904df0e2573543d7a5da90b6c2279d51bd0
Quantompixel <39237785+Quantompixel@users.noreply.github.com>
2023-04-27 15:22:59 +0200

    switch between edit and non edit mode working

--- frontend/src/components/Profile.vue
+++ frontend/src/components/Profile.vue
@@ -1,46 +1,92 @@
 <template>
-  <div class="profile-settings">
-    <nav>
-      <svg @click="test" class="edit-icon" xmlns="http://www.w3.org/2000/svg"
-           viewBox="0 0 139.81 139.31">
-        <path
-            d="M8.05,140.17l17.76-3.39a3.61,3.61,0,0,0,1.87-6.1L13.32,116.32a3.61,3.61,0,0,0-6.1,1.87L3.83,136A3.61,3.61,0,0,0,8.05,140.17Z"
-            transform="translate(-3.76 -0.92)" style="fill:#1d1d1b"/>
-        <path d="M127,49l12.74-12.74a13.09,13.09,0,0,0,0-18.52l-13-13a13.09,13.09,0,0,0-18.52,0L95.5,17.5Z"
-              transform="translate(-3.76 -0.92)" style="fill:#1d1d1b"/>
-        <path d="M117.5,58.5,86,27,14.75,98.25A29,29,0,0,1,18.5,98,28.5,28.5,0,0,1,47,126.5c0,.89,0,1.76-.13,2.63Z"
-              transform="translate(-3.76 -0.92)" style="fill:#1d1d1b"/>
-      </svg>
-    </nav>
-    <div class="edit-profile">
-      <div class="user-data-container">
-        <div class="username">
-          <h2>Benutzername</h2>
-          <p>{{ getUsername }}</p>
+  <div class="profile-wrapper">
+    <div v-if="editingEnabled" class="profile-edit">
+      <nav @click="toggleEdit">
+        <svg class="edit-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
+          <circle cx="25" cy="25" r="25" style="fill:#e73829"/>
+          <rect x="23" y="9.3" width="4" height="30.41" rx="1.24" transform="translate(24.65 -10.5) rotate(45)"
+                style="fill:#f9f9f9"/>
+          <rect x="23" y="9.8" width="4" height="30.41" rx="1.24" transform="translate(60.36 25) rotate(135)"
+                style="fill:#f9f9f9"/>
+        </svg>
+      </nav>
+      <div class="profile">
+        <div class="user-data-container">
+          <div class="username">
+            <h2>Benutzername</h2>
+            <input type="text" :value="getUsername">
+          </div>
+          <div class="email">
+            <h2>E-Mail-Adresse</h2>
+            <p>{{ getEmail }}</p>
+          </div>
         </div>
-        <div class="email">
-          <h2>E-Mail-Adresse</h2>
-          <p>{{ getEmail }}</p>
+        <div class="image-container">
+          <div class="image edit">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 139.81 139.31">
+              <path
+                  d="M8.05,140.17l17.76-3.39a3.61,3.61,0,0,0,1.87-6.1L13.32,116.32a3.61,3.61,0,0,0-6.1,1.87L3.83,136A3.61,3.61,0,0,0,8.05,140.17Z"
+                  transform="translate(-3.76 -0.92)"/>
+              <path d="M127,49l12.74-12.74a13.09,13.09,0,0,0,0-18.52l-13-13a13.09,13.09,0,0,0-18.52,0L95.5,17.5Z"
+                    transform="translate(-3.76 -0.92)"/>
+              <path
+                  d="M117.5,58.5,86,27,14.75,98.25A29,29,0,0,1,18.5,98,28.5,28.5,0,0,1,47,126.5c0,.89,0,1.76-.13,2.63Z"
+                  transform="translate(-3.76 -0.92)"/>
+            </svg>
+          </div>
         </div>
       </div>
-      <div class="image-container">
-        <img class="profile-picture" src="../assets/placeholder_profile.png" alt="Profilbild">
+      <div class="submit-container">
+        <CQButton b-style="login" status="inactive">Speichern</CQButton>
+      </div>
+    </div>
+    <div v-else class="profile-overview">
+      <nav>
+        <svg @click="toggleEdit" class="edit-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 139.81 139.31">
+          <path
+              d="M8.05,140.17l17.76-3.39a3.61,3.61,0,0,0,1.87-6.1L13.32,116.32a3.61,3.61,0,0,0-6.1,1.87L3.83,136A3.61,3.61,0,0,0,8.05,140.17Z"
+              transform="translate(-3.76 -0.92)" style="fill:#1d1d1b"/>
+          <path d="M127,49l12.74-12.74a13.09,13.09,0,0,0,0-18.52l-13-13a13.09,13.09,0,0,0-18.52,0L95.5,17.5Z"
+                transform="translate(-3.76 -0.92)" style="fill:#1d1d1b"/>
+          <path d="M117.5,58.5,86,27,14.75,98.25A29,29,0,0,1,18.5,98,28.5,28.5,0,0,1,47,126.5c0,.89,0,1.76-.13,2.63Z"
+                transform="translate(-3.76 -0.92)" style="fill:#1d1d1b"/>
+        </svg>
+      </nav>
+      <div class="profile">
+        <div class="user-data-container">
+          <div class="username">
+            <h2>Benutzername</h2>
+            <p>{{ getUsername }}</p>
+          </div>
+          <div class="email">
+            <h2>E-Mail-Adresse</h2>
+            <p>{{ getEmail }}</p>
+          </div>
+        </div>
+        <div class="image-container">
+          <div class="image"></div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import CQButton from "@/components/CQButton.vue";
+
 export default {
   name: "Profile",
+  components: {CQButton},
+
   data() {
     return {
-      userData: {}
+      userData: {},
+      editingEnabled: false
     }
   },
   methods: {
-    test() {
-      console.log("test");
+    toggleEdit() {
+      this.editingEnabled = !this.editingEnabled
     },
     fetchData() {
       return fetch(`http://${window.location.hostname}:8080/api/getusers`, {
@@ -89,7 +135,7 @@ export default {
 <style scoped lang="scss">
 @import "/src/assets/colors";
 
-.profile-settings {
+.profile-wrapper {
   border-radius: 10px;
   background: $light_gray;
   padding: 0.5rem 0 1rem 0;
@@ -107,8 +153,9 @@ export default {
     }
   }
 
-  .edit-profile {
+  .profile {
     display: flex;
+    justify-content: space-between;
     width: 100%;
 
     .user-data-container {
@@ -128,23 +175,62 @@ export default {
         overflow: hidden;
         text-overflow: ellipsis;
       }
+
+      input {
+        width: 90%;
+        height: 1.5rem;
+        border: 1px solid $gray;
+        border-radius: 10px;
+        background: white;
+        margin-bottom: 0.5rem;
+        padding-left: 0.5rem;
+
+        &.dirty {
+          border-color: $green;
+
+          &:focus {
+            outline-color: $green;
+          }
+        }
+
+        &.error {
+          border-color: $red;
+
+          &:focus {
+            outline-color: $red;
+          }
+        }
+      }
     }
 
     .image-container {
-      width: 50%;
       padding-right: 1rem;
-      display: flex;
-      justify-content: center;
-      align-items: center;
       overflow: hidden;
 
-      .profile-picture {
+      .image {
+        background-image: url("/src/assets/placeholder_profile.png");
+        background-size: 120px;
+        background-position: center;
+        display: flex;
+        justify-content: center;
+        align-items: center;
         border-radius: 10px;
         width: 100px;
         height: 100px;
         object-fit: cover;
+
+        svg {
+          fill: white;
+          width: 35px;
+          height: 35px;
+        }
       }
     }
+  }
+
+  .submit-container {
+    margin: 1rem auto 0 auto;
+    width: 80%;
   }
 }
 </style>
