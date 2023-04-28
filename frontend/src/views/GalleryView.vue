feacfe82a2d1f98d85f35a38bd1ca94ec2fdc599
Daniel Pillwein <9132@htl.rennweg.at>
2023-04-28 19:39:34 +0200

    little bit of styling

--- frontend/src/views/GalleryView.vue
+++ frontend/src/views/GalleryView.vue
@@ -5,8 +5,8 @@
     </svg>
     <p>1 Bild auswählen</p>
   </div>
-  <div>
-    <div v-for="(image, index) in gallery" :key="index">
+  <div class="image-container">
+    <div class="image" v-for="(image, index) in gallery" :key="index">
       <img :src="image"/>
     </div>
   </div>
@@ -41,7 +41,7 @@ export default {
   left: 0;
   width: 100%;
   height: 70px;
-  background-color: $white;
+  background-color: #eaeaea;
   z-index: 9999;
 
   display: grid;
@@ -62,6 +62,36 @@ export default {
     text-align: center;
     margin: auto 0 auto 0;
     font-size: 1.2rem;
+  }
+}
+
+.image-container{
+  width:100%;
+  margin-top:70px;
+  height: calc(100vh - 140px);
+  background-color: $white;
+  overflow-y: scroll;
+
+  display:flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
+
+  .image{
+    width:45%;
+    margin-top: 15px;
+
+    img{
+      height: 100%;
+      width: 100%;
+      object-fit: cover;
+      padding: 0;
+      margin: 0;
+      border-radius: 10px;
+
+      border: solid $light_gray;
+      box-sizing: border-box;
+    }
   }
 }
 
