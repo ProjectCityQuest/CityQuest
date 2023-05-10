08f5075baa61ee6eb1e2657eb5f2149c7cc0e515
Daniel Pillwein <9132@htl.rennweg.at>
2023-05-10 23:17:20 +0200

    puzzle core done

---
+++ frontend/src/components/PuzzlePiece.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="piece-container" ref="container" :style="{ 'grid-area': gridArea }">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PuzzlePiece",
+  props: {
+    location_name: String,
+    visited: Boolean,
+    image: String,
+    row: Number,
+    column: Number
+  },
+  mounted(){
+    if (this.visited){
+      this.$refs.container.style.backgroundImage  = 'url(' + this.image + ')'
+      //this.$refs.container.style.background = "rgb(" +(this.row * this.column * 2) +","+(this.row * this.column * 2)+","+(this.row * this.column * 2)+")"
+    }
+  },
+  computed:{
+    gridArea(){
+      let row = String.fromCharCode('a'.charCodeAt(0) + (this.row -1 ))
+      let column = String.fromCharCode('a'.charCodeAt(0) + (this.column -1 ))
+      return column + row;
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import "src/assets/colors";
+
+.piece-container{
+  width: 100%;
+  height: 100%;
+  background-color: transparent;
+  border-style: solid;
+  border-width: 1px;
+  box-sizing: border-box;
+  background-position: center;
+  background-size: cover;
+}
+</style>
