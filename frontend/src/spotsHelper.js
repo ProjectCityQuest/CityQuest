e16d09b08cfcfa6f24241a78f3cae5a61e696952
BuildTools <schodllukas8@gmail.com>
2023-05-11 21:08:46 +0200

    removed comments in spotHelper

--- frontend/src/spotsHelper.js
+++ frontend/src/spotsHelper.js
@@ -1,17 +1,13 @@
 let spots = [];
 
 async function setup() {
-    // if (!sessionStorage.getItem("spots")) {
-        await fetch("/src/assets/spots.json")
-            .then(res => res.json())
-            .then(data => spots = data);
+    await fetch("/src/assets/spots.json")
+        .then(res => res.json())
+        .then(data => spots = data);
 
-        spots.forEach(spot => spot.id = "CityQuest" + spot.id);
+    spots.forEach(spot => spot.id = "CityQuest" + spot.id);
 
-        sessionStorage.setItem("spots", JSON.stringify(spots));
-    // } else {
-        // spots = JSON.parse(sessionStorage.getItem("spots"));
-    // }
+    sessionStorage.setItem("spots", JSON.stringify(spots));
 }
 
 export async function getAll() {
