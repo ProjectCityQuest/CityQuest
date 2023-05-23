fe9e80fe3ec989c884936b792e1a353f78936b54
Daniel Pillwein <9132@htl.rennweg.at>
2023-05-23 16:54:58 +0200

    progress

---
+++ frontend/src/components/Slideshow.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="slideshow-container">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Slideshow",
+  components: {
+
+  },
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
