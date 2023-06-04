221dd1e844670b5da26c8452d27980e4fb1089c5
BuildTools <schodllukas8@gmail.com>
2023-06-04 15:53:15 +0200

    auto format

--- frontend/src/views/CollectionView.vue
+++ frontend/src/views/CollectionView.vue
@@ -1,44 +1,47 @@
 <template>
-  <div class="header">
-    <div class="heading" @click="this.sortingOverlay=false">
-      <h1>Sammelbuch</h1>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" class="collection-icon">
-        <path
-            d="M35.21,11.16H14.51a.78.78,0,0,0-.78.78V38.51a.78.78,0,0,0,.78.79h20.7a.79.79,0,0,0,.79-.79V11.94A.78.78,0,0,0,35.21,11.16ZM31.67,21.28,29.06,18a.59.59,0,0,0-.94,0l-2.62,3.3V13.87h6.17Z"/>
-      </svg>
-    </div>
-    <div class="filter">
-      <input type="text" id="searchText" v-model="inputText" name="searchText" placeholder="Suchtext eingeben"
-             @click="this.sortingOverlay=false">
-      <div @click="this.sortingOverlay = !this.sortingOverlay">
-        <svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 96 960 960" width="48">
+  <div class="view-container">
+    <div class="header">
+      <div class="heading" @click="this.sortingOverlay=false">
+        <h1>Sammelbuch</h1>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" class="collection-icon">
           <path
-              d="M322 606V290L202 410l-42-42 193-193 193 193-42 42-122-121v317h-60Zm285 369L414 781l42-41 120 120V544h60v317l122-121 42 42-193 193Z"/>
+              d="M35.21,11.16H14.51a.78.78,0,0,0-.78.78V38.51a.78.78,0,0,0,.78.79h20.7a.79.79,0,0,0,.79-.79V11.94A.78.78,0,0,0,35.21,11.16ZM31.67,21.28,29.06,18a.59.59,0,0,0-.94,0l-2.62,3.3V13.87h6.17Z"/>
         </svg>
       </div>
-      <CQButton @click="filter()" b-style="login" status="active">Suchen</CQButton>
+      <div class="filter">
+        <input type="text" id="searchText" v-model="inputText" name="searchText" placeholder="Suchtext eingeben"
+               @click="this.sortingOverlay=false">
+        <div @click="this.sortingOverlay = !this.sortingOverlay">
+          <svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 96 960 960" width="48">
+            <path
+                d="M322 606V290L202 410l-42-42 193-193 193 193-42 42-122-121v317h-60Zm285 369L414 781l42-41 120 120V544h60v317l122-121 42 42-193 193Z"/>
+          </svg>
+        </div>
+        <CQButton @click="filter()" b-style="login" status="active">Suchen</CQButton>
+      </div>
+      <div class="hr"></div>
     </div>
-    <div class="hr"></div>
-  </div>
-  <div v-if="this.sortingOverlay" class="sortingOverlay">
-    <p class="sort">sortieren nach</p>
-    <div class="options">
-      <p @click="sortEntries(1)">neueste Einträge zuerst</p>
-      <p @click="sortEntries(2)">älteste Einträge zuerst</p>
-      <p @click="sortEntries(3)">Spot-Name absteigend (A-Z)</p>
-      <p @click="sortEntries(4)">Spot-Name aufsteigend (Z-A)</p>
+    <div v-if="this.sortingOverlay" class="sortingOverlay">
+      <p class="sort">sortieren nach</p>
+      <div class="options">
+        <p @click="sortEntries(1)">neueste Einträge zuerst</p>
+        <p @click="sortEntries(2)">älteste Einträge zuerst</p>
+        <p @click="sortEntries(3)">Spot-Name absteigend (A-Z)</p>
+        <p @click="sortEntries(4)">Spot-Name aufsteigend (Z-A)</p>
+      </div>
     </div>
+    <div :class="this.loading?'entry-container loading':'entry-container'" @click="this.sortingOverlay=false">
+      <CollectionEntry v-for="entry in filteredEntries" :id="entry.id" :location="entry.location"
+                       :date="entry.timestamp"
+                       :text="entry.text"
+                       :image="entry.image">
+      </CollectionEntry>
+      <div v-if="loading" class="loading"></div>
+      <div v-else-if="listEmpty" class="empty">Dein Suchtext wurde in keinem deiner Einträge gefunden</div>
+      <div v-else-if="filteredEntries.length===0" class="empty">Du hast noch keine Einträge</div>
+    </div>
+    <NavBar :active-icon="4"></NavBar>
   </div>
-  <div :class="this.loading?'entry-container loading':'entry-container'" @click="this.sortingOverlay=false">
-    <CollectionEntry v-for="entry in filteredEntries" :id="entry.id" :location="entry.location" :date="entry.timestamp"
-                     :text="entry.text"
-                     :image="entry.image">
-    </CollectionEntry>
-    <div v-if="loading" class="loading"></div>
-    <div v-else-if="listEmpty" class="empty">Dein Suchtext wurde in keinem deiner Einträge gefunden</div>
-    <div v-else-if="filteredEntries.length===0" class="empty">Du hast noch keine Einträge</div>
-  </div>
-  <NavBar :active-icon="4"></NavBar>
 </template>
 
 <script>
@@ -88,7 +91,7 @@ export default {
     }
   },
   methods: {
-    getTextFilter(){
+    getTextFilter() {
       if (this.filterText !== "") {
         let filtered = this.entries.filter(entry => entry.text.toUpperCase().includes(this.filterText) || entry.location.toUpperCase().includes(this.filterText))
         this.listEmpty = filtered.length === 0
@@ -138,146 +141,164 @@ export default {
 
 <style scoped lang="scss">
 @import "src/assets/colors";
+@import "src/assets/media_query";
 
-.header {
-  background-color: $white;
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-
-  .heading {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-
-    height: 100px;
-
-    h1 {
-      font-size: 32px;
-    }
-
-    svg {
-      height: 45px;
-      fill: $orange;
-    }
-  }
-
-  .filter {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    margin: 0 auto 0 auto;
-    width: 90%;
-
-    * {
-      height: 2rem;
-    }
-
-    input {
-      border: solid #757575 2px;
-      border-radius: 5px;
-      font-size: 0.9rem;
-      padding-left: 5px;
-      width: 100%;
-    }
-
-    button {
-      border-radius: 5px;
-      text-align: center;
-      max-width: 110px;
-    }
-  }
-
-  .hr {
+.view-container {
+  .header {
+    background-color: $white;
+    position: fixed;
+    top: 0;
+    left: 0;
     width: 100%;
-    height: 2px;
-    background-color: $light_gray;
-    margin-top: 15px;
-  }
 
-}
-
-.sortingOverlay {
-  position: fixed;
-  z-index: 4;
-  left: 5%;
-  top: calc(117px + 2rem + 2px);
-  background-color: $white;
-  border-radius: 0 0 10px 10px;
-  border: solid #d9d9d9 2px;
-
-  .sort {
-    color: #838383;
-    font-size: 0.9rem;
-    padding: 10px 10px 0 10px;
-  }
-
-  .options {
-    * {
-      height: 2.25rem;
+    .heading {
       display: flex;
+      flex-direction: row;
       align-items: center;
-      padding: 0 10px 0 10px;
+      justify-content: center;
 
-      &:hover {
-        background-color: #eaeaea;
+      height: 100px;
+
+      h1 {
+        font-size: 32px;
+      }
+
+      svg {
+        height: 45px;
+        fill: $orange;
+      }
+    }
+
+    .filter {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      margin: 0 auto 0 auto;
+      width: 90%;
+
+      * {
+        height: 2rem;
+      }
+
+      input {
+        border: solid #757575 2px;
+        border-radius: 5px;
+        font-size: 0.9rem;
+        padding-left: 5px;
+        width: 100%;
+      }
+
+      button {
+        border-radius: 5px;
+        text-align: center;
+        max-width: 110px;
+      }
+    }
+
+    .hr {
+      width: 100%;
+      height: 2px;
+      background-color: $light_gray;
+      margin-top: 15px;
+    }
+
+  }
+
+  .sortingOverlay {
+    position: fixed;
+    z-index: 4;
+    left: 5%;
+    top: calc(117px + 2rem + 2px);
+    background-color: $white;
+    border-radius: 0 0 10px 10px;
+    border: solid #d9d9d9 2px;
+
+    .sort {
+      color: #838383;
+      font-size: 0.9rem;
+      padding: 10px 10px 0 10px;
+    }
+
+    .options {
+      * {
+        height: 2.25rem;
+        display: flex;
+        align-items: center;
+        padding: 0 10px 0 10px;
+
+        &:hover {
+          background-color: #eaeaea;
+        }
       }
     }
   }
-}
 
-.entry-container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: calc(117px + 2rem);
-  padding-bottom: 85px;
-  width: 100%;
-  height: calc(100vh - 100px - 2rem);
-  overflow: scroll;
-  background-image: url("../assets/background.png");
-  background-repeat: no-repeat;
-  background-attachment: fixed;
+  .entry-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: calc(117px + 2rem);
+    padding-bottom: 85px;
+    width: 100%;
+    height: calc(100vh - 100px - 2rem);
+    overflow: scroll;
+    background-image: url("../assets/background.png");
+    background-repeat: no-repeat;
+    background-attachment: fixed;
 
 
-  * {
-    width: 90%;
-    margin-top: 15px;
-  }
+    * {
+      width: 90%;
+      margin-top: 15px;
+    }
 
-  &.loading{
-    padding-bottom: 0;
-    justify-content: center;
-  }
+    &.loading {
+      padding-bottom: 0;
+      justify-content: center;
+    }
 
-  .loading{
-    border: 0.8rem solid $light_gray;
-    border-radius: 50%;
-    border-top-color: $blue;
-    width: 50px;
-    height: 50px;
-    margin-top: -70px;
+    .loading {
+      border: 0.8rem solid $light_gray;
+      border-radius: 50%;
+      border-top-color: $blue;
+      width: 50px;
+      height: 50px;
+      margin-top: -70px;
 
-    -webkit-animation: spin 2s linear infinite;
-    animation: spin 2s linear infinite;
-  }
+      -webkit-animation: spin 2s linear infinite;
+      animation: spin 2s linear infinite;
+    }
 
-  .empty {
-    text-align: center;
-    color: $gray;
-    margin: auto 0 auto 0;
+    .empty {
+      text-align: center;
+      color: $gray;
+      margin: auto 0 auto 0;
+    }
   }
 }
 
 @-webkit-keyframes spin {
-  0% { -webkit-transform: rotate(0deg); }
-  100% { -webkit-transform: rotate(360deg); }
+  0% {
+    -webkit-transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+  }
 }
 
 @keyframes spin {
-  0% { transform: rotate(0deg); }
-  100% { transform: rotate(360deg); }
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+@include media-query() {
+  .header {
+    width: $default-min-width;
+    left: auto;
+  }
 }
 </style>
