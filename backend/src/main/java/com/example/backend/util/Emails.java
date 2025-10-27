6fdfec49521aec0302a7d6613cea75750d8c0459
Markus Wizany <markus.wizany@gmail.com>
2025-10-27 19:45:51 +0100

    fix https issue and removed emails

--- backend/src/main/java/com/example/backend/util/Emails.java
+++ backend/src/main/java/com/example/backend/util/Emails.java
@@ -16,7 +16,8 @@ public class Emails {
     private static final Logger LOG = LoggerFactory.getLogger(Emails.class);
 
     public static void sendVerificationMail(String email, String key) throws MessagingException {
-        MimeMessage message = createMailTemplate(email);
+        return;
+        /*MimeMessage message = createMailTemplate(email);
         message.setSubject("Bestätigen Sie Ihre Email!");
 
         EmailTemplate emailVerificationTemplate = new EmailTemplate();
@@ -29,11 +30,12 @@ public class Emails {
 
         Transport.send(message);
 
-        LOG.info("Email Verification sent to '" + email + "' containig key: '" + key + "'");
+        LOG.info("Email Verification sent to '" + email + "' containig key: '" + key + "'");*/
     }
 
     public static void sendPasswordResetMail(String email, String key) throws MessagingException {
-        MimeMessage message = createMailTemplate(email);
+        return;
+        /*MimeMessage message = createMailTemplate(email);
         message.setSubject("Passwortänderung!");
 
         EmailTemplate passwordResetEmailTemplate = new EmailTemplate();
@@ -45,7 +47,7 @@ public class Emails {
         message.setContent(passwordResetEmailTemplate.generateEmailTemplate(), "text/html");
         Transport.send(message);
 
-        LOG.info("Email for password reset sent to '" + email + "' containig key: '" + key + "'");
+        LOG.info("Email for password reset sent to '" + email + "' containig key: '" + key + "'");*/
     }
 
     private static MimeMessage createMailTemplate(String email) throws MessagingException {
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,6 +7,9 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
     base: '/',
     plugins: [vue()],
+    server: {
+        https: true,
+    },
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
